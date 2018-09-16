9c82d0355bb4c11b1e430fd2a328e89e6f919fa6
Felipe Secato <felipe.secato@gmail.com>
2018-09-16 18:12:30 -0300

    vai filhao

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const { WebhookClient, Card, Suggestion } = require('dialogflow-fulfillment')
+const axios = require('axios')
 const app = express()
 
 const wikipediaTemperatureUrl = 'https://en.wikipedia.org/wiki/Temperature';
 const wikipediaTemperatureImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/2/23/Thermally_Agitated_Molecule.gif'
+const weather_api_key = process.env.WEATHER_API_KEY || config.WEATHER_API_KEY
 
 const port = process.env.PORT || 5000
 
@@ -22,6 +24,7 @@ app.post('/dialogflow', (request, response) => {
 
     function welcome(agent) {
         agent.add('Meu webhook favorito')
+
     }
 
     function fallback(agent) {
@@ -31,19 +34,30 @@ app.post('/dialogflow', (request, response) => {
     }
 
     function temperatura(agent) {
-        agent.add(`Welcome to the temperature converter!`);
-        agent.add(new Card({
-            title: `Vibrating molecules`,
-            imageUrl: wikipediaTemperatureImageUrl,
-            text: `Did you know that temperature is really just a measure of how fast molecules are vibrating around?! 😱`,
-            buttonText: 'Temperature Wikipedia Page',
-            buttonUrl: wikipediaTemperatureUrl
-        })
-        );
-        agent.add(`I can convert Celsuis to Fahrenheit and Fahrenheit to Celsius! What temperature would you like to convert?`);
-        agent.add(new Suggestion(`27° Celsius`));
-        agent.add(new Suggestion(`-40° Fahrenheit`));
-        agent.add(new Suggestion(`Cancel`));
+        const latitude = -20.8467
+        const longtitude = -41.1202
+        const api_url = `https://api.darksky.net/forecast/${weather_api_key}/${latitude},${longtitude}`
+        console.log(api_url)
+        axios.get(api_url)
+            .then(result => {
+                const temperature = (result.data.currently.temperature - 32) / 1.8
+                const apparentTemperature = (result.data.currently.apparentTemperature - 32) / 1.8
+                agent.add(`A temperatura agora em cachoeiro é de ${temperatura} graus celsius.`)
+                // agent.add(`Welcome to the temperature converter!`);
+                // agent.add(new Card({
+                //     title: `Vibrating molecules`,
+                //     imageUrl: wikipediaTemperatureImageUrl,
+                //     text: `Did you know that temperature is really just a measure of how fast molecules are vibrating around?! 😱`,
+                //     buttonText: 'Temperature Wikipedia Page',
+                //     buttonUrl: wikipediaTemperatureUrl
+                // })
+                // );
+                // agent.add(`I can convert Celsuis to Fahrenheit and Fahrenheit to Celsius! What temperature would you like to convert?`);
+                // agent.add(new Suggestion(`27° Celsius`));
+                // agent.add(new Suggestion(`-40° Fahrenheit`));
+                // agent.add(new Suggestion(`Cancel`));
+            }).catch(err => console.error(err))
+
     }
 
     let intentMap = new Map();
