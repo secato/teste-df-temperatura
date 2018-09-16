756e6e55cfa7dfc2793faaa8d85b98053e3eab35
Felipe Secato <felipe.secato@gmail.com>
2018-09-16 16:45:10 -0300

    teste

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const app = express()
 
 const port = process.env.PORT || 5000
 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/dialogflow', (req, res) => {
-    console.log(req)
+    console.log(req.body)
 })
 
 app.listen(port, console.log(`Server started listening on ${port}`))
