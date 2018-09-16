793e4978572526b68f80dd9c2dcdfa878f683874
Felipe Secato <felipe.secato@gmail.com>
2018-09-16 16:49:45 -0300

    teste

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.post('/dialogflow', (req, res) => {
     console.log(req.body)
