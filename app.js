14d7cac139d15392b2a73f768df400930de1945a
Felipe Secato <felipe.secato@gmail.com>
2018-09-16 16:41:07 -0300

    webhook post

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/dialogflow', (req, res) => {
+app.post('/dialogflow', (req, res) => {
     res.send(req.body)
 })
 
